8f648cf8c258ce5424258cf75369b8fcd8945a9c
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-03 15:03:20 +0200

    add comments

---
+++ dadi/js/main.js
@@ -0,0 +1,20 @@
+console.log("it works");
+
+//1)Generare un numero random da 1 a 6:
+// a)sia per il giocatore;
+// b)sia per il computer;
+
+
+//Stabilire il vincitore: 
+//condizione: in base a chi fa il punteggio più alto.(1.punteggio più alto--> Win; 2. punteggio più basso --> lose; 3. stesso punteggio --> equality)
+
+
+
+
+
+
+
+
+
+//Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
