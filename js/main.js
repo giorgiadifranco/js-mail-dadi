6efc7b81a4e1948a6ebbd080c5a8ca56162c03fd
giorgiadifranco <giorgiadf08@gmail.com>
2024-10-03 14:51:54 +0200

    email

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 console.log("it works");
 
 
+//Chiedi all’utente la sua email
+//lista di chi può accedere
+//controlla (verify) che sia nella lista di chi può accedere
+//stampa un messaggio appropriato sull’esito del controllo.
+
+
+let emailRequest = prompt("Inserisci la tua mail");
+console.log(emailRequest);
+
+let mailList = ["andrea123@gmail.com","alt_mail@gmail.com", "marco_123@gmail.com"];
+
+let emailIn = emailRequest;
+
+
+
+
+if(mailList.includes(emailIn)) {
+
+    let message = "Benvenuto";
+    console.log(message);
+    
+}else if(mailList.includes(emailIn)){
+    let message = "Email non presente";
+    console.log(message);
+    
+}
